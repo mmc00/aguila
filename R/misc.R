fd8fa65a483779b0d27eda33b8f4a8977ceabf9c
Marlon <dracomarmol@gmail.com>
2020-08-28 10:48:09 -0600

    Update misc.R

--- R/misc.R
+++ R/misc.R
@@ -1,50 +1,50 @@
 # debuggin firts function
 # data <- data_list[["WITS 2018"]]
 # data_name <- "WITS 2018"
-# lista_data <- clean_data(path = "data/dataset_indicadores_19082020.xlsx")
+lista_data <- clean_data(path = "data/dataset_indicadores_28082020.xlsx")
 # data2 <- lista_data %>% 
 #   bind_rows
 # 
 # data = lista_data[[1]]
 # 
 # 
-# data = data_list2[[13]]
-# data_name = names(data_list2[13])
-# path = "data/dataset_indicadores_19082020.xlsx"
-# countries_names_var = c(
-#                           "Country", "Region/economy",
-#                           "Reporter Name", "Country Name",
-#                           "Country/Territory", "País",
-#                           "Country", "...2"
-#                         )
-# code_names = c("Code", "CODE")
-#                        data_cols_years = c(
-#                           "Human Development Index (HDI)",
-#                           "Inflows FDI",
-#                           "WITS 2018",
-#                           "WB", "Telecomm"
-#                         )
-# control_id_var = c(
-#                          "Indicator",
-#                          "Series Code",
-#                          "Indicador"
-#                          )
-# two_headers = c(
-#                           "VoiceandAccountability",
-#                           "RegulatoryQuality",
-#                           "ControlofCorruption",
-#                           "GovernmentEffectiveness",
-#                           "RuleofLaw"
-#                         )
-# var_in_cols_type_sheet = c(
-#                          "DB", "FTA CR"
-#                        )
-#                        var_to_remove_in_cols = c(
-#                          "Country code", "Region",
-#                          "Income group"
-#                        )
-# var_year = c(
-#                          "DB Year",
-#                          "Year of entry into force"
-#                        )
-                      
+data = data_list2[[13]]
+data_name = names(data_list2[13])
+path = "data/dataset_indicadores_19082020.xlsx"
+countries_names_var = c(
+                          "Country", "Region/economy",
+                          "Reporter Name", "Country Name",
+                          "Country/Territory", "País",
+                          "Country", "...2"
+                        )
+code_names = c("Code", "CODE")
+                       data_cols_years = c(
+                          "Human Development Index (HDI)",
+                          "Inflows FDI",
+                          "WITS 2018",
+                         "WITS completo 2017",
+                          "WB", "Telecomm"
+                        )
+control_id_var = c(
+                         "Indicator",
+                         "Series Code",
+                         "Indicador"
+                         )
+two_headers = c(
+                          "VoiceandAccountability",
+                          "RegulatoryQuality",
+                          "ControlofCorruption",
+                          "GovernmentEffectiveness",
+                          "RuleofLaw"
+                        )
+var_in_cols_type_sheet = c(
+                         "DB", "FTA CR"
+                       )
+                       var_to_remove_in_cols = c(
+                         "Country code", "Region",
+                         "Income group"
+                       )
+var_year = c(
+                         "DB Year",
+                         "Year of entry into force"
+                       )
