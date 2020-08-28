cc7450d3ad074b62df16dcc98779d4f12e772086
Marlon <dracomarmol@gmail.com>
2020-08-28 14:39:14 -0600

    Create filtering_data.R

---
+++ R/filtering_data.R
@@ -0,0 +1,23 @@
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##'
+##' @title
+##' @param dat
+filtering_data <- function(dat,
+                           path_map = "auxi/map.xlsx",
+                           map_countries_sheet = "region",
+                           drop_vars_map_sheet = "varsdrop") {
+  
+  # read maps
+  map_countries <- read.xlsx(path_map, sheet = map_countries_sheet)
+  drop_vars <- read.xlsx(path_map, sheet = drop_vars_map_sheet)
+  # filtering
+  data <- dat %>% 
+    left_join(map_countries, by = "country_code") %>% 
+    left_join(drop_vars, by = c("variable",	"grouping_var")) %>% 
+    filter(!is.na(region)) %>% 
+    filter(is.na(drop))
+  
+  return(data)
+}
