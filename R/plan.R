6196c8109e34b340ac53ac5d07c08100040f5441
Marlon <dracomarmol@gmail.com>
2020-08-28 13:26:58 -0600

    Update plan.R

--- R/plan.R
+++ R/plan.R
@@ -3,6 +3,8 @@ the_plan <-
 
     long_data =
       clean_data(path = 
-                   file_in("data/dataset_indicadores_28082020.xlsx"))
+                   file_in("data/dataset_indicadores_28082020.xlsx")),
+    data_country = 
+      fixing_countries_names(long_data)
 
     )
