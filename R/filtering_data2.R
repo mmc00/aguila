563ff088482c30004d27a0d88168076531bdaea7
Marlon <dracomarmol@gmail.com>
2020-09-11 15:09:28 -0600

    Create filtering_data2.R

---
+++ R/filtering_data2.R
@@ -0,0 +1,59 @@
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##'
+##' @title
+##' @param dat
+filtering_data2 <- function(dat,
+                           path_map = "auxi/map2.xlsx",
+                           drop_vars_map_sheet = "varsdrop",
+                           country_var_sheet = "country_var",
+                           select_year = 2017,
+                           output) {
+  # read maps
+  drop_vars <- read_xlsx(path_map, sheet = drop_vars_map_sheet)
+  country_var <- read_xlsx(path_map, sheet = country_var_sheet)
+  # filtering
+  data <- dat %>% 
+    left_join(drop_vars, by = c("variable",	"grouping_var")) %>% 
+    filter(is.na(drop)) %>% 
+    select(-drop) %>% 
+    left_join(country_var, by = c("country",	"grouping_var")) %>% 
+    filter(is.na(drop2)) %>% 
+    select(-drop2)
+  # changing estimates and with grouping Variable
+  data <- data %>% 
+    mutate(variable =case_when(
+      variable == "Estimate" ~ grouping_var,
+      TRUE ~ variable
+    ))
+  # summary data
+  data_year_sum <- data %>% 
+  group_by(variable, grouping_var, country_code) %>%
+  summarise(
+    max_year = max(year),
+    min_year = min(year), .groups = "drop"
+  ) %>%
+  mutate(years = paste0(min_year, "_", max_year)) %>%
+  select(variable, grouping_var, country_code, years) %>%
+  pivot_wider(names_from = country_code, values_from = years)
+  # summary nas
+  data_wide <- data %>%
+    filter(year == select_year) %>%
+    select(-country_code, -region, -grouping_var, -year) %>% 
+    pivot_wider(names_from = "variable",
+                values_from = "values"
+                #values_fn = length
+                ) %>% 
+    mutate(count_na = rowSums(is.na(.)))
+  # export
+  wb <- createWorkbook()
+  addWorksheet(wb, "data")
+  addWorksheet(wb, "summary")
+  addWorksheet(wb, "data_wide")
+  writeData(wb, "data", data)
+  writeData(wb, "summary", data_year_sum)
+  writeData(wb, "data_wide", data_wide)
+  saveWorkbook(wb, file = output, overwrite = T)
+  return(data_wide)
+}
