1820c824fdcdf37f8ac555e8200e9e40a58689a5
Marlon <dracomarmol@gmail.com>
2020-10-01 21:10:49 -0600

    Update plan.R
    dynamic plan with exo tariff (WITS)

--- R/plan.R
+++ R/plan.R
@@ -17,11 +17,20 @@ the_plan <-
     # adding WEF, RTA and WB data to data filtered
     data_merge = wef_rta_wb_join(data_filtered, 
                                  file_in("data/wef_rta_wb.xlsx")),
-    
-    # second filter 
-    data2fil = filtering_data2(data_merge,
-                           path_map = file_in("auxi/map2.xlsx"),
-                           output = file_out("auxi/data2fil.xlsx"),
-                           select_year = 2016)
 
-    )
+    # year selected for analysis
+    year = 2016,
+    
+    # second filter
+    data2fil = target(filtering_data2(data_merge,
+                           path_map = file_in("auxi/map2.xlsx"),
+                           select_year = year),
+                dynamic = map(year)),
+    
+    # tariff data complete
+    tariff = target(tariff_wits(path = file_in(
+      "data/DataJobID-2059520_2059520_aguila2.csv"
+    ),
+    select_year = year
+    ), dynamic = map(year))
+)
